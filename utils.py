a83eb1d9957d44320fda8f4cb387fefc30e28dc0
north-redwing <narita9226@gmail.com>
2020-12-07 17:32:50 +0900

    add cat/utils

---
+++ utils.py
@@ -0,0 +1,31 @@
+import os
+
+import librosa
+import numpy as np
+import soundfile as sf
+
+def padding(dir_name, window_length=128*128, time_stretch=False):
+    os.chdir(dir_name)
+    for phase in ['train', 'valid']:
+        os.chdir(phase)
+        print(os.getcwd())
+        fps = os.listdir()
+        for fp in fps:
+            if fp.endswith('wav'):
+                audio, sr = librosa.load(fp, 16000)
+                if len(audio) < window_length:
+                    audio = np.append(audio, [0.0] * (window_length-len(audio)))
+                    assert len(audio) == window_length
+                    sf.write(fp, audio, 16000)
+                elif len(audio) == window_length:
+                    pass
+                else:
+                    print(fp)
+                    rate = len(audio) / window_length
+                    audio = librosa.effects.time_stretch(audio, rate)
+                    assert len(audio) == window_length
+                    sf.write(fp, audio, 16000)
+        os.chdir('..')
+
+if __name__ == '__main__':
+    padding('cat', time_stretch=True)
