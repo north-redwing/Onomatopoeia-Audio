db7e5c98a91bcb7d3d0f71ca29ea52cb2fd7b6ce
north-redwing <narita9226@gmail.com>
2020-12-09 18:38:01 +0900

    initial commit

--- utils.py
+++ utils.py
@@ -27,5 +27,29 @@ def padding(dir_name, window_length=128*128, time_stretch=False):
                     sf.write(fp, audio, 16000)
         os.chdir('..')
 
+def stretch(dir_name, window_length=128*128):
+    os.chdir(dir_name)
+    for phase in ['train', 'valid']:
+        os.chdir(phase)
+        print(os.getcwd())
+        fps = os.listdir()
+        for fp in fps:
+            if fp.endswith('wav'):
+                audio, sr = librosa.load(fp, 16000)
+                if len(audio) < window_length:
+                    rate = len(audio) / window_length
+                    audio = librosa.effects.time_stretch(audio, rate)
+                    assert len(audio) == window_length
+                    sf.write(fp, audio, 16000)
+                elif len(audio) == window_length:
+                    pass
+                else:
+                    audio = audio[:window_length]
+                    assert len(audio) == window_length
+                    sf.write(fp, audio, 16000)
+        os.chdir('..')
+
+
 if __name__ == '__main__':
-    padding('cat', time_stretch=True)
+    # padding('cat', time_stretch=True)
+    stretch('car_horn')
