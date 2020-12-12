5f2c7db51c229f7e13be7b7c1857ee2fda558993
north-redwing <narita9226@gmail.com>
2020-12-12 14:56:03 +0900

    add not cut mode for stretch

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import os
-
 import librosa
 import numpy as np
 import soundfile as sf
@@ -27,7 +26,7 @@ def padding(dir_name, window_length=128*128, time_stretch=False):
                     sf.write(fp, audio, 16000)
         os.chdir('..')
 
-def stretch(dir_name, window_length=128*128):
+def stretch(dir_name, window_length=128*128, cut=True):
     os.chdir(dir_name)
     for phase in ['train', 'valid']:
         os.chdir(phase)
@@ -43,13 +42,19 @@ def stretch(dir_name, window_length=128*128):
                     sf.write(fp, audio, 16000)
                 elif len(audio) == window_length:
                     pass
-                else:
-                    audio = audio[:window_length]
-                    assert len(audio) == window_length
-                    sf.write(fp, audio, 16000)
+                elif len(audio) > window_length:
+                    if cut:
+                        audio = audio[:window_length]
+                        assert len(audio) == window_length
+                        sf.write(fp, audio, 16000)
+                    else:
+                        rate = len(audio) / window_length
+                        audio = librosa.effects.time_stretch(audio, rate)
+                        assert len(audio) == window_length
+                        sf.write(fp, audio, 16000)
         os.chdir('..')
 
 
 if __name__ == '__main__':
     # padding('cat', time_stretch=True)
-    stretch('car_horn')
+    stretch('cow', cut=False)
